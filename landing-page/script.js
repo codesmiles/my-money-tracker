759b4f3bb1a794d58ccc2f2d1d59eb95ea416c1d
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2021-06-23 21:14:26 +0100

    Update on resopnsiveness abd begining on interactiveness

---
+++ landing-page/script.js
@@ -0,0 +1,21 @@
+let email2 = document.querySelector(".email-2");
+let enterEmail = document.getElementById('enter-email');
+let submitSubscriptionInfo = document.getElementsByClassName('sub-btn');
+
+
+
+
+ submitSubscriptionInfo.addEventListener(click, ()=>{
+   
+
+    
+ })
+
+
+ enterEmail.addEventListener(input, ()=>{
+     if(enterEmail.Value.length !== 0){
+
+         email2.style.display = 'none'
+        
+     }
+ })
